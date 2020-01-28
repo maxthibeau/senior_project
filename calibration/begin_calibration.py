fa80972254009fe6355614094bb59bfde629bfcd
maxthibeau <thibeaumax@gmail.com>
2020-01-28 10:43:53 -0700

    cleaned up begin_calibration

--- calibration/begin_calibration.py
+++ calibration/begin_calibration.py
@@ -1,6 +1,5 @@
 import sys
 from ConfigFile import *
-from MeterologicalInput import *
 from PFTSelector import *
 import h5py
 
@@ -10,9 +9,7 @@ def main(argv):
     exit(1)
   config_fname = argv[0]
   config_file = ConfigFile(config_fname)
-  # FIXME: load meterological input dataset from config file
-  fname = "../DataFiles/L4C_meteorological_input_dataset.h5"
-  meteor_input = MeterologicalInput(h5py.File(fname, 'r'))
+  meteor_input = config_file.meteorological_input()
   pft_selected = PFTSelector.select_pft(meteor_input)
 
 if __name__ == "__main__":
