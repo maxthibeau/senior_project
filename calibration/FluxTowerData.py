a8aba40d3a3e3793ca9419e960a77b4c302d8ce5
maxthibeau <thibeaumax@gmail.com>
2020-01-29 12:03:57 -0700

    small bug fix

--- calibration/FluxTowerData.py
+++ calibration/FluxTowerData.py
@@ -7,11 +7,6 @@ class FluxTowerData():
   def __init__(self, flux_tower_dir):
     self._fluxes = []
     for filename in os.listdir(flux_tower_dir):
-<<<<<<< HEAD
-      full_file_path = flux_tower_dir +'/'+ filename
-      with open(full_file_path) as f:
-        pass
-=======
       full_file_path = flux_tower_dir + filename
       self._fluxes.append(csv.reader(full_file_path))
     self._fluxes = np.array(self._fluxes)
@@ -21,4 +16,3 @@ class FluxTowerData():
 
   def take(self, keys):
     return np.take(self._fluxes, keys)
->>>>>>> 2a57d8d262e2c689a0bd19d66fafadf94211d314
