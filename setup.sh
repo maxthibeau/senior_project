80f6576ef0347b77f5e2b2d8ae7530f19f14a885
Jake Pennington <jake.pennington@umontana.edu>
2020-04-04 16:27:13 -0600

    Fixed gdal intallation in setup script

--- setup.sh
+++ setup.sh
@@ -2,13 +2,9 @@
 
 sudo apt-get install python3 -y
 sudo apt-get install python-pyqt5 -y
-
 sudo -H python3 -m pip install matplotlib
 sudo -H python3 -m pip install scipy
 sudo -H python3 -m pip install numpy
 sudo -H python3 -m pip install h5py
 sudo -H python3 -m pip install affine
-
-# gdal
-sudo apt-get install aptitude -y
-sudo aptitude install libgdal-dev -y
+sudo apt-get install python3-gdal -y
