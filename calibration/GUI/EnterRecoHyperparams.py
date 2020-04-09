d493cc253d6d2584479e2a6c8422cf1c18e9eddc
Lucas Hamilton <lucas1.hamilton@umconnect.umt.edu>
2020-04-09 08:38:52 -0600

    Changed how error message works on this page and made some general aesthetic changes

--- calibration/GUI/EnterRecoHyperparams.py
+++ calibration/GUI/EnterRecoHyperparams.py
@@ -14,6 +14,9 @@ class EnterRecoHyperparams(BasePage):
     self.Prh_label = QtWidgets.QLabel("Prh:")
     self.Prh_label.setToolTip('Percentile of RH/Kmult to use in SOC pools')
     self.Prh_label.setFont(QtGui.QFont("Times", 10))
+    self.Prh_error_label = QtWidgets.QLabel("Error: Prh must be between 0 and 1")
+    self.Prh_error_label.setFont(QtGui.QFont("Times", 10))
+    self.Prh_error_label.setVisible(False)
     self.Prh_textbox = QtWidgets.QLineEdit(self)
     self.Prh_textbox.textChanged.connect(self.updatePrhslide)
     self.Prh_slide = QtWidgets.QSlider(QtCore.Qt.Horizontal)
@@ -22,15 +25,20 @@ class EnterRecoHyperparams(BasePage):
     self.Prh_slide.setTickInterval(10)
     self.Prh_slide.setTickPosition(QtWidgets.QSlider.TicksBelow)
     self.Prh_slide.setSingleStep(10)
+    self.Prh_slide.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
     self.Prh_slide.valueChanged.connect(self.valuePrhchange)
     Prh_layout = QtWidgets.QGridLayout()
     Prh_layout.addWidget(self.Prh_label,1,0,1,0)
     Prh_layout.addWidget(self.Prh_slide,1,0,2,2)
     Prh_layout.addWidget(self.Prh_textbox,1,2,2,4)
+    Prh_layout.addWidget(self.Prh_error_label,2,0,1,0)
 
     self.Pk_label = QtWidgets.QLabel("Pk:")
     self.Pk_label.setToolTip('Percentile to use as minimum threshold for acceptable Kmult values')
     self.Pk_label.setFont(QtGui.QFont("Times", 10))
+    self.Pk_error_label = QtWidgets.QLabel("Error: Pk must be between 0 and 1")
+    self.Pk_error_label.setFont(QtGui.QFont("Times", 10))
+    self.Pk_error_label.setVisible(False)
     self.Pk_textbox = QtWidgets.QLineEdit(self)
     self.Pk_textbox.textChanged.connect(self.updatePkslide)
     self.Pk_slide = QtWidgets.QSlider(QtCore.Qt.Horizontal)
@@ -39,18 +47,26 @@ class EnterRecoHyperparams(BasePage):
     self.Pk_slide.setTickInterval(10)
     self.Pk_slide.setTickPosition(QtWidgets.QSlider.TicksBelow)
     self.Pk_slide.setSingleStep(10)
+    self.Pk_slide.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
     self.Pk_slide.valueChanged.connect(self.valuePkchange)
     Pk_layout = QtWidgets.QGridLayout()
     Pk_layout.addWidget(self.Pk_label,1,0,1,0)
     Pk_layout.addWidget(self.Pk_slide,1,0,2,2)
     Pk_layout.addWidget(self.Pk_textbox,1,2,2,4)
-
+    Pk_layout.addWidget(self.Pk_error_label,2,0,1,0)
+    
     # move on to next page
     button_layout = QtWidgets.QHBoxLayout()
     prev_button = QtWidgets.QPushButton("Redisplay GPP Parameter Difference")
+    prev_button.setFixedSize(250,75)
+    prev_button.setFont(QtGui.QFont("Times", 8, QtGui.QFont.Bold))
+    prev_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
     prev_button.clicked.connect(self.prev_page)
-    next_button = QtWidgets.QPushButton("Display RECO RAMP Functions")
+    next_button = QtWidgets.QPushButton("Proceed")
     next_button.setToolTip('Continue')
+    next_button.setFont(QtGui.QFont("Times", 9, QtGui.QFont.Bold))
+    next_button.setFixedSize(250,75)
+    next_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
     next_button.clicked.connect(self.next_page)
     button_layout.addWidget(prev_button)
     button_layout.addWidget(next_button)
@@ -59,8 +75,8 @@ class EnterRecoHyperparams(BasePage):
     main_layout.addWidget(page_label,1,0,1,0,alignment=Qt.AlignHCenter)
     main_layout.addWidget(self.pft_label,1,0,2,0,alignment=Qt.AlignHCenter)
     main_layout.addLayout(Prh_layout,2,0,4,0)
-    main_layout.addLayout(Pk_layout,4,0,6,0)
-    main_layout.addLayout(button_layout,6,0,6,0)
+    main_layout.addLayout(Pk_layout,5,0,4,0)
+    main_layout.addLayout(button_layout,7,0,6,0)
     self.setLayout(main_layout)
     self.setWindowTitle(page_title)
 
@@ -91,13 +107,21 @@ class EnterRecoHyperparams(BasePage):
     Prh = self.Prh_textbox.text()
     Pk = self.Pk_textbox.text()
     if not self.valid_value(Prh):
-      self.Prh_label.setText("Prh needs to be a float between 0 and 1")
-      self.Prh_label.setStyleSheet("color: red;")
+      self.Prh_error_label.setText("Prh needs to be a float between 0 and 1")
+      self.Prh_error_label.setStyleSheet("color: red;")
+      self.Prh_error_label.setVisible(True)
 
     if not self.valid_value(Pk):
-      self.Pk_label.setText("Pk needs to be a float between 0 and 1")
-      self.Pk_label.setStyleSheet("color: red;")
-
+      self.Pk_error_label.setText("Pk needs to be a float between 0 and 1")
+      self.Pk_error_label.setStyleSheet("color: red;")
+      self.Pk_error_label.setVisible(True)
+      
+    if(self.valid_value(Prh)):
+      self.Prh_error_label.setVisible(False)
+      
+    if(self.valid_value(Pk)):
+      self.Pk_error_label.setVisible(False)
+      
     if self.valid_value(Prh) and self.valid_value(Pk):
       self.next_window.emit()
       self.hide()
