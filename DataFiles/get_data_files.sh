dc998ae909ed9ad071903ed1eab485707cfc847e
markm700 <mm249676@umconnect.umt.edu>
2020-04-09 11:12:22 -0600

    added file to get_data_files.sh

--- DataFiles/get_data_files.sh
+++ DataFiles/get_data_files.sh
@@ -5,6 +5,7 @@ wget http://files.ntsg.umt.edu/data/SKYentists/L4C_calibration_inputs/Fluxnet201
 wget http://files.ntsg.umt.edu/data/SKYentists/L4C_calibration_inputs/L4_C_input_NRv72_Vv4xxxx_smapMergedCalVal.h5 -nc &
 wget http://files.ntsg.umt.edu/data/SKYentists/L4C_calibration_inputs/202002/calibSites_Natv72.csv -nc &
 wget http://files.ntsg.umt.edu/data/SKYentists/L4C_calibration_inputs/igbp_soc_M09smapMergedCalVal.flt32 -nc &
+wget http://files.ntsg.umt.edu/data/SKYentists/L4C_calibration_inputs/202002/L4_C_input_NRv72_for_356_tower_sites_smrz100.h5 -nc &
 
 wait
 
