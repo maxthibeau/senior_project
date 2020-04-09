2ec036e6877dbbb1702f6a4ddf3451a42334ae11
Lucas Hamilton <lucas1.hamilton@umconnect.umt.edu>
2020-04-09 06:27:09 -0600

    Changed layout of the parameters to make it look a little nicer, as well as some general sizing and aligning

--- calibration/GUI/ParameterChoosing.py
+++ calibration/GUI/ParameterChoosing.py
@@ -12,22 +12,33 @@ class ParameterChoosing(BasePage):
        self.param_label = QtWidgets.QLabel("7. "+gpp_or_reco+" Optimization Parameters")
     else:
        self.param_label = QtWidgets.QLabel("11. "+gpp_or_reco+" Optimization Parameters")
-    self.param_label.setFont(QtGui.QFont("Times", 13))
-    self.param_label.move(0,-100)
+    self.param_label.setFont(QtGui.QFont("Times", 18))
+    #self.param_label.move(0,-100)
     self.pft_label = QtWidgets.QLabel("Current PFT: "+self.pft_chooser(1)) #TODO: change to get correct pft ind
-    self.pft_label.setFont(QtGui.QFont("Times", 11))
-    self.pft_label.move(0,-250)
+    self.pft_label.setFont(QtGui.QFont("Times", 15))
+    
+    self.top_layout = QtWidgets.QVBoxLayout()
+    self.top_layout.addWidget(self.param_label,alignment=Qt.AlignCenter)
+    self.top_layout.addWidget(self.pft_label,alignment=Qt.AlignHCenter | Qt.AlignBottom)
+    self.top_layout.setAlignment(Qt.AlignCenter)
 
     for param in self.params:
       checkbox = QtWidgets.QCheckBox(param)
+      checkbox.setFont(QtGui.QFont("Times", 10))
+      #Source: https://stackoverflow.com/questions/41784184/how-to-resize-qcheckbox
+      checkbox.setStyleSheet("QCheckBox::indicator { width: 25px; height: 25px; }") #May have to change the check image to an image of a higher resolution
       if param in tooltip:
           checkbox.setToolTip(tooltip[param])
       self.param_checkboxes.append(checkbox)
 
-    self.select_parameters_label = QtWidgets.QLabel("Select Parameters To Optimize:")
+    self.select_parameters_label = QtWidgets.QLabel("Select Parameters To Optimize")
+    self.select_parameters_label.setFont(QtGui.QFont("Times", 11,QtGui.QFont.Bold))
+    self.select_parameters_label.setAlignment(Qt.AlignHCenter)
     #self.select_parameters_label.setStyleSheet("text-decoration: underline;")
 
     grid_layout = QtWidgets.QGridLayout()
+    grid_layout.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+    grid_layout.setHorizontalSpacing(20)
     for i in range(len(self.param_checkboxes)):
       param_checkbox = self.param_checkboxes[i]
       num_checkboxes = len(self.param_checkboxes)
@@ -36,23 +47,30 @@ class ParameterChoosing(BasePage):
         grid_layout.addWidget(param_checkbox, i, 0)
       else:
         grid_layout.addWidget(param_checkbox, i - num_checkboxes / 2, 1)
-
+    
+    self.mid_layout = QtWidgets.QVBoxLayout()
+    self.mid_layout.addWidget(self.select_parameters_label,alignment=Qt.AlignHCenter)
+    self.mid_layout.addLayout(grid_layout)
+    self.mid_layout.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+    
     self.optimize_button = QtWidgets.QPushButton("Optimize Parameters")
+    self.optimize_button.setFixedSize(250,75)
+    self.optimize_button.setFont(QtGui.QFont("Times", 9, QtGui.QFont.Bold))
     self.optimize_button.clicked.connect(self.next_page)
     self.prev_page_button = QtWidgets.QPushButton("Re-display old RAMP functions")
+    self.prev_page_button.setFixedSize(250,75)
+    self.prev_page_button.setFont(QtGui.QFont("Times", 9, QtGui.QFont.Bold))
     self.prev_page_button.clicked.connect(self.prev_page)
 
-    choose_layout = QtWidgets.QGridLayout()
-    choose_layout.addWidget(self.select_parameters_label,1,1,1,2)
-    choose_layout.addLayout(grid_layout,2,1,4,2)
-    choose_layout.addWidget(self.prev_page_button,2,3,2,1)
-    choose_layout.addWidget(self.optimize_button,4,3,2,1)
+    button_layout = QtWidgets.QHBoxLayout()
+    button_layout.addWidget(self.prev_page_button)
+    button_layout.addWidget(self.optimize_button)
 
-    main_layout = QtWidgets.QGridLayout()
-    main_layout.setSpacing(5)
-    main_layout.addWidget(self.param_label,1,0,1,0,alignment=Qt.AlignHCenter)
-    main_layout.addWidget(self.pft_label,1,0,2,0,alignment=Qt.AlignHCenter)
-    main_layout.addLayout(choose_layout,2,0,4,0)
+    main_layout = QtWidgets.QVBoxLayout()
+    #main_layout.setSpacing(5)
+    main_layout.addLayout(self.top_layout)
+    main_layout.addLayout(self.mid_layout)
+    main_layout.addLayout(button_layout)
     self.setLayout(main_layout)
     self.setWindowTitle(page_title)
 
